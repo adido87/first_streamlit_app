e11fa785de9d96badbe6ac27c30632d45b8ffce2
adido87 <133859695+adido87@users.noreply.github.com>
2023-05-17 11:26:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Paents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Hard-Boiled Free-Range Egg')
