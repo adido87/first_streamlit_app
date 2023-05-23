2682e76f3b84d8f29719ff1d7dddce8186e290f0
adido87 <133859695+adido87@users.noreply.github.com>
2023-05-23 11:06:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.dataframe(my_data_rows)
 
 streamlit.text('What fruit would you like to add?')
 #add_my_fruit = streamlit.text_input("label goes here", default_value_goes_here)
-
+add_my_fruit = st.text_input(fruit = "What fruit would you like to add ?" )
