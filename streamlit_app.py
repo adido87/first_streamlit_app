1f2d3cc86a1b0b62be8ec0b9a285bfa71c3bf2d9
adido87 <133859695+adido87@users.noreply.github.com>
2023-05-23 11:10:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,5 @@ streamlit.text("The fruit load list contains :")
 streamlit.dataframe(my_data_rows)
 
 streamlit.text('What fruit would you like to add?')
-add_my_fruit = streamlit.text_input(fruit = "What fruit would you like to add ?" )
+#add_my_fruit = streamlit.text_input(fruit = "What fruit would you like to add ?" )
+add_my_fruit = text_input(label = "Please enter the path to the file" )
